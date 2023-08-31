89b9b7bdf8ac2264f12d79c769d785af58069049
jiepengtan <jiepengtan@gmail.com>
2023-08-31 18:55:04 +0800

    [*] update assimp lib

---
+++ Dependencies/assimp/include/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: I:/Projects/assimp-master/contrib/zlib
+# Build directory: I:/Projects/assimp-master/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
