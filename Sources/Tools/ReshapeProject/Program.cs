b0af345a7b8882d06f6133fdd75d41997a7d73d5
jiepengtan <jiepengtan@gmail.com>
2023-09-03 18:27:07 +0800

    [*] update code header replace tools

--- Sources/Tools/ReshapeProject/Program.cs
+++ Sources/Tools/ReshapeProject/Program.cs
@@ -1,39 +1,110 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using GamesTan;
 
-
-class Application
+namespace ReshapeProject
 {
-    static int Main()
+    static class StringExt
     {
-        var paths = new string[]
+        static bool StartWithExt(this string str, string tag)
         {
-            @"I:\Projects\OverloadEngine\Overload\Sources\Runtime",
-            @"I:\Projects\OverloadEngine\Overload\Sources\Game",
-            @"I:\Projects\OverloadEngine\Overload\Sources\Editor",
-        };
-        List<string> allCppFiles = new List<string>();
-        foreach (var dir in paths)
+            if (str.Length < tag.Length) return false;
+            var len = tag.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if (str[i] != tag[i]) return false;
+            }
+
+            return true;
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
         {
-            PathUtil.Walk(dir,"*.h|*.inl", (p) =>
+            var paths = new string[]
             {
-                allCppFiles.Add(p);
-            });
-            PathUtil.Walk(dir,"*.h", (p) =>
+                @"../../Sources/Game",
+                @"../../Sources/Runtime",
+                @"../../Sources/Editor",
+            };
+            var namespaceMap = new Dictionary<string, string>()
             {
-                allCppFiles.Add(p);
-            });
-            PathUtil.Walk(dir,"*.cpp", (p) =>
-            {
-                allCppFiles.Add(p);
-            });
+                { "OvAnalytics", "Core/Analytics" },
+                { "OvAudio", "Modules/Audio" },
+                { "OvDebug", "Core/Debug" },
+                { "OvEditor", "../Editor" },
+                { "OvGame", "../Game" },
+                { "OvMaths", "Core/Maths" },
+                { "OvPhysics", "Modules/Physics" },
+                { "OvRendering", "Resource/Rendering" },
+                { "OvTools", "Core/Tools" },
+                { "OvUI", "Modules/UI" },
+                { "OvWindowing", "Platform/Windowing" },
+                { "OvAnim", "Modules/Anim" },
+                { "OvCore/ResourceManagement", "Resource/Rendering" },
+                { "OvCore/Resources", "Resource/Resources" },
+                { "OvCore/GlobalState.h", "Core/GlobalState.h" },
+                { "OvCore/", "GamePlay/" },
+            };
+
+
+            ReplaceCodeIncludes(paths, namespaceMap);
         }
 
-        foreach (var file in allCppFiles)
+        
+        private static void ReplaceCodeIncludes(string[] paths, Dictionary<string, string> namespaceMap)
         {
-            Console.WriteLine(file);
+            List<string> allCppFiles = new List<string>();
+            foreach (var dir in paths)
+            {
+                PathUtil.Walk(dir, "*.h|*.inl", (p) => { allCppFiles.Add(p); });
+                PathUtil.Walk(dir, "*.h", (p) => { allCppFiles.Add(p); });
+                PathUtil.Walk(dir, "*.cpp", (p) => { allCppFiles.Add(p); });
+            }
+
+            var headTag = "#include";
+            var oldIncludeTag = "Ov";
+            foreach (var file in allCppFiles)
+            {
+                Console.WriteLine(file);
+                var lines = File.ReadAllLines(file);
+                var resultLines = new List<string>(lines.Length);
+                foreach (var line in lines)
+                {
+                    var result = line;
+                    var trimLine = line.Trim();
+                    var isHead = trimLine.StartsWith(headTag);
+                    if (isHead)
+                    {
+                        var head = trimLine.Replace(headTag, "").Trim()
+                            .Replace("\"", "")
+                            .Replace("<", "")
+                            .Replace(">", "")
+                            .Replace("\\", "/")
+                            .Trim();
+                        if (head.StartsWith(oldIncludeTag)) // old namespace
+                        {
+                            foreach (var item in namespaceMap)
+                            {
+                                if (head.StartsWith(item.Key))
+                                {
+                                    head = head.Replace(item.Key, item.Value);
+                                    break;
+                                }
+                            }
+
+                            result = $"{headTag} \"{head}\"";
+                        }
+                    }
+
+                    resultLines.Add(result);
+                }
+
+                File.WriteAllLines(file, resultLines);
+            }
         }
-        return 1;
     }
 }
