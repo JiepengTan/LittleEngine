9b6ed517f1c32d9c595c896fc92b0092a7642d90
jiepengtan <jiepengtan@gmail.com>
2023-09-06 08:46:52 +0800

    [*] code refactor,Extract json11 & imgui as a third-party library

--- Sources/3rdParty/imgui/ImGui/imgui_demo.cpp
+++ Sources/3rdParty/imgui/ImGui/imgui_demo.cpp
@@ -66,7 +66,7 @@ Index of this file:
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 
-#include "Modules/UI/ImGui/imgui.h"
+#include "imgui.h"
 #ifndef IMGUI_DISABLE
 
 #include <ctype.h>          // toupper
