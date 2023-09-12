00996c28b31a5cf928df2c8744d4675ca86e0e8c
jiepengtan <jiepengtan@gmail.com>
2023-09-12 17:22:30 +0800

    [*] add test components

--- Sources/Runtime/_Test/Component/TestComp2.h
+++ Sources/Runtime/_Test/Component/TestComp2.h
@@ -14,5 +14,26 @@ namespace LittleEngine
             LOG_INFO("Hello world TestComp2");
         }
         void OnInspectorGUI() override;
+    private:
+        META(OnEnable)
+        float dataFloat;
+        META(OnEnable)
+        FVector2 dataVec2;
+        META(OnEnable)
+        FVector3 dataVec3;
+        META(OnEnable)
+        FVector4 dataVec4;
+        META(OnEnable)
+        Color dataColor;
+        META(OnEnable)
+        TextureResPtr ptr1;
+        META(OnEnable)
+        ModelResPtr ptr2;
+        META(OnEnable)
+        SoundResPtr ptr3;
+        META(OnEnable)
+        MaterialResPtr  ptr4;
+        META(OnEnable)
+        ShaderResPtr ptr5;
     };
 }
